ed345899c494f9ec93780f8bc818ed28b84b6ff0
Arefin21 <arefinshagor18@gmail.com>
2025-12-30 13:46:59 +0600

    Initial commit with full project including vendor

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+require __DIR__.'/auth.php';
 Route::get('/{shortCode}', [ShortenUrlController::class, 'redirectToOriginal'])->name('shortened.redirect');
 
-require __DIR__.'/auth.php';
+
